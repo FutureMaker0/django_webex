7fabd3b4f94d1ca3b518dc12d8a77e957e3fa170
FutureMaker0 <120623320+FutureMaker0@users.noreply.github.com>
2023-07-13 17:38:20 +0900

    update_0713

--- app/urls.py
+++ app/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 
 urlpatterns = [
+    path('', views.IndexMain.as_view()),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    # path('user/', include('user.urls')),
+    
+
 ]
