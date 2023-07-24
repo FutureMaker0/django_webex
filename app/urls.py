9aa754ae4df09956e543262f9d706a3e8e476977
FutureMaker0 <120623320+FutureMaker0@users.noreply.github.com>
2023-07-24 16:55:32 +0900

    settings/urls.py 수정 _media 경로지정 및 urlpatterns 추가

--- app/urls.py
+++ app/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.IndexMain.as_view(), name='main'),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('user/', include('user.urls')),
-
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
