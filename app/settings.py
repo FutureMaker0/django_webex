6db9661cc73bdc9c7d46378025204ce4a29cae79
JaeHyeonKim <120623320+FutureMaker0@users.noreply.github.com>
2023-09-11 16:28:04 +0900

    env 환경변수 설정 - 키 암호화

--- app/settings.py
+++ app/settings.py
@@ -12,9 +12,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 AUTH_USER_MODEL = 'user.User'
 
@@ -23,8 +30,9 @@ AUTH_USER_MODEL = 'user.User'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d2$%xf69d-gntc7_yw-qw$8#g4_t5!a3sdd)(_%+b^#dw=wpd2'
+# # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
